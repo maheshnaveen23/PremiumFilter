f22d56ed35a3e1cced2bd1041f11d23725e81aa5
maheshnaveen23 <135042499+maheshnaveen23@users.noreply.github.com>
2023-06-03 14:36:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/maheshnaveen23/PremiumFilter.git /PremiumFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PiroAutoFilterBot
